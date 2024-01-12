0eb029b0222216e1181fac67814125908ad0bf17
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2024-01-13 01:08:48 +0900

    実行完了のcommit

--- WebBeginnerStarter/src/main/java/com/example/demo/app/SampleController.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/app/SampleController.java
@@ -1,7 +1,9 @@
 package com.example.demo.app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sample")
