4ac6d9e8a11749eaa0e25b8ee75bc0b4ef24037d
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2024-01-17 18:39:18 +0900

    34 演習内容

--- WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,18 +1,27 @@
 package com.example.demo.app.survey;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.service.SurveyService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/*
- * Add annotations here
- */
+import javax.validation.Valid;
+import org.slf4j.Logger;
+
+@Controller
+@RequestMapping("/survey")
 public class SurveyController {
 	
 	private final SurveyService surveyService;
-	
+
 	public SurveyController(SurveyService surveyService){
 		this.surveyService = surveyService;
 	}
@@ -24,12 +33,12 @@ public class SurveyController {
 		
 		return "survey/index";
 	}
-	
+
 	@GetMapping("/form")
-	public String form(/*Add parameters.*/) {
-		
-		//hands-on
-		
+	public String form(SurveyForm surveyForm,
+					   Model model,
+					   @ModelAttribute("complete") String complete) {
+		model.addAttribute("title", "Survey Form");
 		return "survey/form";
 	}
 	
@@ -43,19 +52,29 @@ public class SurveyController {
 	
 	
 	@PostMapping("/confirm")
-	public String confirm(/*Add parameters.*/) {
-		
-		//hands-on
-		
+	public String confirm(
+			@Validated SurveyForm surveyForm,
+			BindingResult result,
+			Model model) {
+		if(result.hasErrors())	{
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
+		}
+		model.addAttribute("title", "Confirm Page");
 		return "survey/confirm";
 	}
 	
 	@PostMapping("/complete")
-	public String complete(/*Add parameters.*/) {
-		
-		//hands-on
-		
-		return "";
+	public String complete(
+			@Valid @ModelAttribute SurveyForm surveyForm,
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
+		}
+		redirectAttributes.addFlashAttribute("complete", "Thanks for your cooperation!");
+		return "redirect:/survey/form";
 	}
-	
 }
